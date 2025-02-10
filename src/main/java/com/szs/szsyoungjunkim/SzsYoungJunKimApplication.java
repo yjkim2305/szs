46eff3b9e21fb9b8953e04a8324441cc2cba8927
KimYoungJun <cyberact3181@gmail.com>
2025-02-10 15:35:44 +0900

    feat: 프로젝트 초기 셋팅

---
+++ src/main/java/com/szs/szsyoungjunkim/SzsYoungJunKimApplication.java
@@ -0,0 +1,13 @@
+package com.szs.szsyoungjunkim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SzsYoungJunKimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SzsYoungJunKimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/szs/szsyoungjunkim/SzsYoungJunKimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szs.szsyoungjunkim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SzsYoungJunKimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
