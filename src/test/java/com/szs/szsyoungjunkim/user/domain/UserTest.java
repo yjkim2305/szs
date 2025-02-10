649ef1abb190ff3b9acac1b4b590f1fd0b2fce1a
KimYoungJun <cyberact3181@gmail.com>
2025-02-10 22:32:15 +0900

    feat: user 도메인 테스트

---
+++ src/test/java/com/szs/szsyoungjunkim/user/domain/UserTest.java
@@ -0,0 +1,76 @@
+package com.szs.szsyoungjunkim.user.domain;
+
+import com.szs.szsyoungjunkim.common.exception.CoreException;
+import com.szs.szsyoungjunkim.user.application.dto.UserCreateCommand;
+import com.szs.szsyoungjunkim.user.domain.exception.UserErrorType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp() {
+        passwordEncoder = new BCryptPasswordEncoder();
+    }
+
+    @Test
+    @DisplayName("허용된 사용자는 예외 없이 validateUsers를 통과해야 한다.")
+    void validateUsers_Success() {
+        //given
+        User user = User.builder()
+                .userId("dongtak123")
+                .password("password")
+                .name("동탁")
+                .regNo("921108-1582816")
+                .build();
+
+        //when & then
+        assertDoesNotThrow(() -> user.validateUsers(user.getName(), user.getRegNo()));
+    }
+
+    @Test
+    @DisplayName("허용되지 않는 사용자는 예외가 발생해야 한다.")
+    void validateUsers_Fail() {
+        //given
+        User user = User.builder()
+                .userId("dongtak123")
+                .password("password")
+                .name("동탁")
+                .regNo("921108-1582817")
+                .build();
+
+        //when
+        CoreException exception = assertThrows(CoreException.class, () -> {
+            user.validateUsers(user.getName(), user.getRegNo());
+        });
+
+        //then
+        assertEquals(UserErrorType.INVALID_USER.getMessage(), exception.getMessage());
+        assertEquals(UserErrorType.INVALID_USER.name(), exception.getErrorType().getErrorCode());
+    }
+
+    @Test
+    @DisplayName("비밀번호가 암호화된 상태로 User 객체가 정상적으로 생성되어야 한다")
+    void createWithEncodedPassword() {
+        // given
+        UserCreateCommand command = new UserCreateCommand("user123", "securePassword", "유비", "790411-1656116");
+
+        // when
+        User user = User.createWithEncodedPassword(command, passwordEncoder);
+
+        // then
+        assertThat(user).isNotNull();
+        assertThat(user.getUserId()).isEqualTo("user123");
+        assertThat(user.getName()).isEqualTo("유비");
+        assertThat(user.getRegNo()).isEqualTo("790411-1656116");
+
+        assertThat(passwordEncoder.matches("securePassword", user.getPassword())).isTrue();
+    }
+}
