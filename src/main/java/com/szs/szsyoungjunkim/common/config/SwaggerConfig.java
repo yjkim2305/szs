efa8b90ba9c68e374554a5bc0fa5ca03200be8d6
KimYoungJun <cyberact3181@gmail.com>
2025-02-11 15:46:10 +0900

    feat: swagger jwt 인증 추가

---
+++ src/main/java/com/szs/szsyoungjunkim/common/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.szs.szsyoungjunkim.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .name("Authorization")
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
