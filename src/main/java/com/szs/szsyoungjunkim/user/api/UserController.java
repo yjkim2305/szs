94141e5cfbd8e1848a0622c6b1a507e41b472240
KimYoungJun <cyberact3181@gmail.com>
2025-02-13 11:17:44 +0900

    feat: 로그인 회원가입 API swagger 작성

--- src/main/java/com/szs/szsyoungjunkim/user/api/UserController.java
+++ src/main/java/com/szs/szsyoungjunkim/user/api/UserController.java
@@ -11,12 +11,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "UserController", description = "사용자 회원가입, 로그인 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/szs")
