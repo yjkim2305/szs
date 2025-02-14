90a7ad2200b318f1bc2f3c39c8b164a12816fc48
KimYoungJun <cyberact3181@gmail.com>
2025-02-14 09:27:58 +0900

    fix: deductionfacade 개선

---
+++ src/main/java/com/szs/szsyoungjunkim/deduction/application/service/ScrapService.java
@@ -0,0 +1,28 @@
+package com.szs.szsyoungjunkim.deduction.application.service;
+
+import com.szs.szsyoungjunkim.common.exception.CoreException;
+import com.szs.szsyoungjunkim.deduction.domain.exception.DeductionErrorType;
+import com.szs.szsyoungjunkim.deduction.feign.ScrapClient;
+import com.szs.szsyoungjunkim.deduction.feign.request.ScrapRequest;
+import com.szs.szsyoungjunkim.deduction.feign.response.ClientApiRes;
+import com.szs.szsyoungjunkim.deduction.feign.response.ScrapResponse;
+import com.szs.szsyoungjunkim.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ScrapService {
+    private final ScrapClient scrapClient;
+
+    public ScrapResponse getScrapData(User user) {
+        ClientApiRes<ScrapResponse> scrapResponse = scrapClient.scrapData(ScrapRequest.of(user.getName(), user.getRegNo()));
+
+        if (scrapResponse == null || scrapResponse.getData() == null) {
+            throw new CoreException(DeductionErrorType.INVALID_RESPONSE_VALUE);
+        }
+
+        return scrapResponse.getData();
+    }
+
+}
--- src/main/java/com/szs/szsyoungjunkim/deduction/facade/DeductionFacade.java
+++ src/main/java/com/szs/szsyoungjunkim/deduction/facade/DeductionFacade.java
@@ -2,10 +2,8 @@ package com.szs.szsyoungjunkim.deduction.facade;
 
 import com.szs.szsyoungjunkim.common.exception.CoreException;
 import com.szs.szsyoungjunkim.deduction.application.service.DeductionService;
+import com.szs.szsyoungjunkim.deduction.application.service.ScrapService;
 import com.szs.szsyoungjunkim.deduction.domain.exception.DeductionErrorType;
-import com.szs.szsyoungjunkim.deduction.feign.ScrapClient;
-import com.szs.szsyoungjunkim.deduction.feign.request.ScrapRequest;
-import com.szs.szsyoungjunkim.deduction.feign.response.ClientApiRes;
 import com.szs.szsyoungjunkim.deduction.feign.response.ScrapResponse;
 import com.szs.szsyoungjunkim.refund.application.service.RefundService;
 import com.szs.szsyoungjunkim.refund.application.service.dto.RefundCreateCommand;
@@ -21,30 +19,26 @@ public class DeductionFacade {
 
     private final UserService userService;
     private final DeductionService deductionService;
-    private final ScrapClient scrapClient;
+    private final ScrapService scrapService;
     private final RefundService refundService;
 
     @Transactional
     public void userScrap(String userId) {
         User user = userService.findByUserId(userId);
 
-        ClientApiRes<ScrapResponse> scrapResponse = scrapClient.scrapData(ScrapRequest.of(user.getName(), user.getRegNo()));
+        ScrapResponse scrapResponse = scrapService.getScrapData(user);
 
-        if (scrapResponse == null || scrapResponse.getData() == null) {
-            throw new CoreException(DeductionErrorType.INVALID_RESPONSE_VALUE);
-        }
-
-        Integer taxYear = scrapResponse.getData().deduction().creditCardDeductionResponse().year();
+        Integer taxYear = scrapResponse.deduction().creditCardDeductionResponse().year();
 
         if (deductionService.existsByUserIdAndTaxYear(userId, taxYear) || refundService.existsByUserIdAndTaxYear(userId, taxYear)) {
             throw new CoreException(DeductionErrorType.EXIST_TAX_YEAR_DEDUCTION);
         }
 
-        deductionService.saveDeductions(scrapResponse.getData().deduction(), userId);
+        deductionService.saveDeductions(scrapResponse.deduction(), userId);
 
         refundService.saveRefund(RefundCreateCommand.of(taxYear,
-                scrapResponse.getData().totalIncomeTax(),
-                scrapResponse.getData().deduction().taxCredit(),
+                scrapResponse.totalIncomeTax(),
+                scrapResponse.deduction().taxCredit(),
                 userId));
     }
 }
